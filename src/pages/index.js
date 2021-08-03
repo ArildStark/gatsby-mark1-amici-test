62d28712c43ca24a53e449c8eaf9b3dc082e5d5e
ArildStark <87885663+ArildStark@users.noreply.github.com>
2021-08-03 17:08:56 +0800

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import NavHeader from "../components/header"
+import Herosection from "../components/heroimg"
+import Aboutsection from "../components/aboutsection"
+import ProductSection from "../components/products-section"
+import Homefaqsection from "../components/homefaqsection"
+import Homecontactsection from "../components/homecontactsection"
+
+
+import Seo from "../components/seo"
+
+
+const home  = () =>  {
+
+return(
+
+<html>
+
+<NavHeader>
+</NavHeader>
+<Herosection>
+</Herosection>
+<Aboutsection></Aboutsection>
+<ProductSection></ProductSection>
+<Homefaqsection></Homefaqsection>
+<Homecontactsection></Homecontactsection>
+
+
+
+
+
+
+
+
+
+</html>
+
+)
+
+}
+
+export default home
